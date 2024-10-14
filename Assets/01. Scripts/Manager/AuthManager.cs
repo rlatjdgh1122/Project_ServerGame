1e1c6eea993a6df6dcd9ec05a68655e1c651def8
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-10-14 19:03:13 +0900

    로그인 씬

--- Assets/01. Scripts/Manager/AuthManager.cs
+++ Assets/01. Scripts/Manager/AuthManager.cs
@@ -1,49 +1,73 @@
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class AuthManager : MonoBehaviour
 {
-    public TextMeshProUGUI _logText = null;
-    public TextMeshProUGUI token = null;
+	public TextMeshProUGUI _logText = null;
+	public TextMeshProUGUI token = null;
 
-    async void Start()
-    {
-        await UnityServices.InitializeAsync();
-    }
+	async void Start()
+	{
+		await UnityServices.InitializeAsync();
+	}
 
-    public async void OnSignIn()
-    {
-        await SignInAnonymous();
-    }
+	public async void OnSignIn()
+	{
+		await SignInAnonymous();
+	}
 
-    public void OnSignOut()
-    {
-        _logText.text = "pleaze Click to LoginButton";
-        token.text = $"토큰 : {GetAccessToken()}";
-        AuthenticationService.Instance.SignOut(true);
-    }
+	public void OnSignOut()
+	{
+		_logText.text = "pleaze Click to LoginButton";
+		//token.text = $"토큰 : {GetAccessToken()}";
+		AuthenticationService.Instance.SignOut(true);
+	}
 
-    private async Task SignInAnonymous()
-    {
-        try
-        {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            token.text = $"토큰 : {GetAccessToken()}";
-            _logText.text = $"Player Id : {AuthenticationService.Instance.PlayerId}";
-        }
-        catch (AuthenticationException ex)
-        {
-            _logText.text = "Sign in Failed!";
-            Debug.LogException(ex);
-        }
-    }
+	private async Task SignInAnonymous()
+	{
+		try
+		{
+			//await AuthenticationService.Instance.SignInAnonymouslyAsync();
+			//token.text = $"토큰 : {GetAccessToken()}";
+			//_logText.text = $"Player Id : {AuthenticationService.Instance.PlayerId}";
 
-    public string GetAccessToken()
-    {
-        return AuthenticationService.Instance.AccessToken;
+			await VerifyTokenWithServer(GetAccessToken());
+		}
+		catch (AuthenticationException ex)
+		{
+			_logText.text = "Sign in Failed!";
+			Debug.LogException(ex);
+		}
+	}
+
+	public string GetAccessToken()
+	{
+		return AuthenticationService.Instance.AccessToken;
+	}
+
+	private async Task VerifyTokenWithServer(string accessToken)
+	{
+		using (HttpClient client = new HttpClient())
+		{
+			client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+
+			string serverUrl = "https://localhost:7012/api/Auth/verifyToken";
+			HttpResponseMessage response = await client.GetAsync(serverUrl);
+
+			if (response.IsSuccessStatusCode)
+			{
+				Debug.Log("Token verification succeeded");
+			}
+			else
+			{
+				Debug.LogError("Token verification failed");
+			}
+		}
     }
 }
